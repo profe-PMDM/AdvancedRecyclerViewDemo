f802b4f43259fe34b66c749c8c9e3f99b1d07f6f
David Corcuera <david.corcuera@gmail.com>
2023-02-12 21:17:34 +0100

    add DataBinding to Item List View

--- app/src/main/java/com/example/advancedrecyclerviewdemo2023/NumberListAdapter.kt
+++ app/src/main/java/com/example/advancedrecyclerviewdemo2023/NumberListAdapter.kt
@@ -1,28 +1,27 @@
 package com.example.advancedrecyclerviewdemo2023
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.advancedrecyclerviewdemo2023.databinding.ItemViewBinding
 
-class NumberListAdapter() :
+class NumberListAdapter :
     ListAdapter<Int,NumberListAdapter.IntViewHolder>(RowItemDiffCallback())  {
 
-    class IntViewHolder(val row: View): RecyclerView.ViewHolder(row) {
-        val textView: TextView = row.findViewById(R.id.number)
-    }
+    class IntViewHolder (val binding: ItemViewBinding):
+        RecyclerView.ViewHolder(binding.root)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): IntViewHolder {
-        val layout = LayoutInflater.from(parent.context)
-            .inflate(R.layout.item_view, parent, false)
-        return IntViewHolder(layout)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ItemViewBinding.inflate(layoutInflater, parent, false)
+        return IntViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: IntViewHolder, position: Int) {
-        holder.textView.text = getItem(position).toString()
+        val number = getItem(position)
+        holder.binding.num = number
     }
 }
 
