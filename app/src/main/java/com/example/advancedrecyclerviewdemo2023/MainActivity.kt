a735fbae5aaf60e33abe3aab22c1935ecf40f756
David Corcuera <david.corcuera@gmail.com>
2023-02-12 22:17:36 +0100

    add clickListener to items

--- app/src/main/java/com/example/advancedrecyclerviewdemo2023/MainActivity.kt
+++ app/src/main/java/com/example/advancedrecyclerviewdemo2023/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.example.advancedrecyclerviewdemo2023
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.advancedrecyclerviewdemo2023.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val adapter = NumberListAdapter()
+    private val adapter = NumberListAdapter(MyListener { num ->
+        Toast.makeText(this,
+            "El número seleccionado es el ${num}",
+            Toast.LENGTH_LONG)
+            .show()
+    })
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.rv.adapter = adapter
-        binding.rv.layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
+        binding.rv.layoutManager = LinearLayoutManager(this)
 
         adapter.submitList(IntRange(0,100).toList())
 
--- app/src/main/java/com/example/advancedrecyclerviewdemo2023/NumberListAdapter.kt
+++ app/src/main/java/com/example/advancedrecyclerviewdemo2023/NumberListAdapter.kt
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.example.advancedrecyclerviewdemo2023.databinding.ItemViewBinding
 
-class NumberListAdapter :
+class NumberListAdapter(val clickListener: MyListener) :
     ListAdapter<Int,NumberListAdapter.IntViewHolder>(RowItemDiffCallback())  {
 
     class IntViewHolder (val binding: ItemViewBinding):
@@ -22,6 +22,7 @@ class NumberListAdapter :
     override fun onBindViewHolder(holder: IntViewHolder, position: Int) {
         val number = getItem(position)
         holder.binding.num = number
+        holder.binding.clickListener = clickListener
     }
 }
 
@@ -34,5 +35,9 @@ class RowItemDiffCallback : DiffUtil.ItemCallback<Int>() {
     override fun areContentsTheSame(oldItem: Int, newItem: Int): Boolean {
         return oldItem == newItem
     }
+}
+
+class MyListener(val clickListener: (num: Int) -> Unit) {
+    fun onClick(num: Int) = clickListener(num)
 }
 
